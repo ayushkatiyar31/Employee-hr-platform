6d5f2ce0a02b5c2b83909aa010821b1b08322f84
ayushkatiyar31 <143071145+ayushkatiyar31@users.noreply.github.com>
2026-05-19 22:57:03 +0530

    make changes

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const mongo_url = process.env.MONGODB_URI || 'mongodb://localhost:27017/employee_hr_platform';
 
+
 mongoose.connect(mongo_url)
     .then(() => {
         console.log('MongoDB Connected to:', mongo_url.replace(/\/\/.*@/, '//***:***@'))
